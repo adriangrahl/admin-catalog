e6024ec3726c3281845b5adb4235d110f418face
Adrian Pereira <adrian.pereira@senior.com.br>
2023-01-27 08:35:30 -0300

    Implementing DDD Tactical Pattern classes: ValueObject, Identifier, Entity and AggregateRoot

---
+++ domain/src/main/java/br/com/codeflix/admin/catalog/domain/AggregateRoot.java
@@ -0,0 +1,9 @@
+package br.com.codeflix.admin.catalog.domain;
+
+public class AggregateRoot<ID extends Identifier> extends Entity<ID>{
+
+    protected AggregateRoot(final ID id) {
+        super(id);
+    }
+
+}
---
+++ domain/src/main/java/br/com/codeflix/admin/catalog/domain/ValueObject.java
@@ -0,0 +1,4 @@
+package br.com.codeflix.admin.catalog.domain;
+
+public abstract class ValueObject {
+}
