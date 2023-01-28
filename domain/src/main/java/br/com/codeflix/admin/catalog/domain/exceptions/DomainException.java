92ad3ce2449cdf2039761bb2ba42439c7b59e2c8
Adrian Pereira <adrian.pereira@senior.com.br>
2023-01-28 13:15:19 -0300

    NoStackTraceException to improve performance of DomainExceptions (writableStackTrace = false)

--- domain/src/main/java/br/com/codeflix/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/br/com/codeflix/admin/catalog/domain/exceptions/DomainException.java
@@ -3,17 +3,21 @@ package br.com.codeflix.admin.catalog.domain.exceptions;
 import br.com.codeflix.admin.catalog.domain.validation.Error;
 import java.util.List;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStackTraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> theErrors) {
-        super("", null, true, false);
+    private DomainException(final String aMessage, final List<Error> theErrors) {
+        super(aMessage);
         this.errors = theErrors;
     }
 
+    public static DomainException with(final Error anError) {
+        return new DomainException(anError.message(), List.of(anError));
+    }
+
     public static DomainException with(final List<Error> theErrors) {
-        return new DomainException(theErrors);
+        return new DomainException("", theErrors);
     }
 
     public List<Error> getErrors() {
