15233d277011bfb830c3c03d72f2de49058224f2
Adrian Pereira <adrian.pereira@senior.com.br>
2023-01-29 16:23:34 -0300

    Mockito junit jupiter

--- application/src/test/java/br/com/codeflix/admin/catalog/application/UseCaseTest.java
+++ application/src/test/java/br/com/codeflix/admin/catalog/application/UseCaseTest.java
@@ -2,15 +2,17 @@ package br.com.codeflix.admin.catalog.application;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
     @Test
     public void testCreateUseCase() {
-        Assertions.assertNotNull(new UseCase());
-//        Assertions.assertNotNull(new UseCase().execute());
+        
     }
 
 }
