884a69cfc4f67637afc56ffbbeba2aeee907e82c
Adrian Pereira <adrian.pereira@senior.com.br>
2023-01-31 00:09:49 -0300

    Setup SpringBoot at the infrastructure layer

--- infrastructure/src/main/java/br/com/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/br/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -1,9 +1,13 @@
 package br.com.codeflix.admin.catalog.infrastructure;
 
-import br.com.codeflix.admin.catalog.application.UseCase;
+import br.com.codeflix.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        SpringApplication.run(WebServerConfig.class, args);
         System.out.println("Hello world!");
 //        System.out.println(new UseCase().execute());
     }
---
+++ infrastructure/src/main/java/br/com/codeflix/admin/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package br.com.codeflix.admin.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.com.codeflix.admin.catalog")
+public class WebServerConfig {
+}
