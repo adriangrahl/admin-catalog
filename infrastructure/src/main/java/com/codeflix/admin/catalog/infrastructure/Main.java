7d55bf1beedd15bcb8e95da8f7c80110941abe41
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2025-05-06 23:46:07 -0300

    feat: added Category class

---
+++ infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -0,0 +1,12 @@
+package com.codeflix.admin.catalog.infrastructure;
+
+import com.codeflix.admin.catalog.application.UseCase;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new UseCase().execute());
+        System.out.println("Hello World!");
+    }
+}
