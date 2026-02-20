5cc106f2b52c3d29096eed9c5f7a7a1b7d081e5c
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-02-19 21:48:14 -0300

    feat: add mockito; UseCase<IN,OUT>; UnitUseCase<IN>; NullaryUseCase<OUT>; Command, Output, UseCase and DefaultUseCase for Category

---
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/create/CreateCategoryOutput.java
@@ -0,0 +1,13 @@
+package com.codeflix.admin.catalog.application.category.create;
+
+import com.codeflix.admin.catalog.domain.category.Category;
+import com.codeflix.admin.catalog.domain.category.CategoryID;
+
+public record CreateCategoryOutput(
+        CategoryID id
+) {
+
+    public static CreateCategoryOutput from(final Category aCategory) {
+        return new CreateCategoryOutput(aCategory.getId());
+    }
+}
---
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/create/CreateCategoryUseCase.java
@@ -0,0 +1,7 @@
+package com.codeflix.admin.catalog.application.category.create;
+
+import com.codeflix.admin.catalog.application.UseCase;
+
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+
+}
