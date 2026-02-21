619a2bc8c0d7011844f903d22b72963ebfef9aff
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-02-21 20:51:02 -0300

    feat: add spring boot starter data jpa, mysql-connector-java (driver) and docker-compose to startup mysql locally

--- infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -3,12 +3,14 @@ package com.codeflix.admin.catalog.infrastructure;
 import com.codeflix.admin.catalog.infrastructure.configuration.WebServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfiguration.class, args);
     }
 
