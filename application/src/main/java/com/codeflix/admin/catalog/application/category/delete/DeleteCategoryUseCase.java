2e93276e87c124786433792569d6d2b73e4509df
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-02-21 11:14:25 -0300

    feat: add delete category use case and tests

---
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.codeflix.admin.catalog.application.category.delete;
+
+import com.codeflix.admin.catalog.application.UnitUseCase;
+
+public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
+}
