7caa9f65b48c2b9d1541217128fcce6486c8eee6
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-02-21 13:41:18 -0300

    feat: add getCategoryById use case and tests

---
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/retrieve/get/DefaultGetCategoryByIdUseCase.java
@@ -0,0 +1,32 @@
+package com.codeflix.admin.catalog.application.category.retrieve.get;
+
+import com.codeflix.admin.catalog.domain.category.CategoryGateway;
+import com.codeflix.admin.catalog.domain.category.CategoryID;
+import com.codeflix.admin.catalog.domain.exceptions.DomainException;
+import com.codeflix.admin.catalog.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryByIdUseCase extends GetCategoryByIdUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultGetCategoryByIdUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public CategoryOutput execute(final String anId) {
+        final var aCategoryId = CategoryID.from(anId);
+        return this.categoryGateway.findById(aCategoryId)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(aCategoryId));
+    }
+
+    private Supplier<DomainException> notFound(final CategoryID anId) {
+        return () -> DomainException.with(
+                new Error("Category with ID %s was not found".formatted(anId.getValue()))
+        );
+    }
+}
---
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.codeflix.admin.catalog.application.category.retrieve.get;
+
+import com.codeflix.admin.catalog.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+}
