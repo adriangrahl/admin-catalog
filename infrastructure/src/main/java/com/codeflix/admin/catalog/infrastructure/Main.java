7bbf830d9833bcd300e3a9526ec15763a1bd2db4
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-02-22 13:08:51 -0300

    feat: add JpaRepository (auto-implemented by SimpleJpaRepository)

--- infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/codeflix/admin/catalog/infrastructure/Main.java
@@ -1,9 +1,16 @@
 package com.codeflix.admin.catalog.infrastructure;
 
+import com.codeflix.admin.catalog.domain.category.Category;
+import com.codeflix.admin.catalog.infrastructure.category.persistence.CategoryJpaEntity;
+import com.codeflix.admin.catalog.infrastructure.category.persistence.CategoryRepository;
 import com.codeflix.admin.catalog.infrastructure.configuration.WebServerConfiguration;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -12,6 +19,18 @@ public class Main {
         System.out.println("Hello World!");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfiguration.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository) {
+        return args -> {
+            List<CategoryJpaEntity> all = repository.findAll();
+
+            Category filmes = Category.newCategory("filmes", null, true);
+
+            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+            repository.deleteAll();
+        };
     }
 
 }
