98a7644fae4e30845fb48fbba73eeb35da4413c1
Adrian Grahl Pereira <adrian.grahl@gmail.com>
2026-03-01 15:27:04 -0300

    feat: category api init

--- infrastructure/src/test/java/com/codeflix/admin/catalog/application/category/retrieve/list/ListCategoriesUseCaseIT.java
+++ infrastructure/src/test/java/com/codeflix/admin/catalog/application/category/retrieve/list/ListCategoriesUseCaseIT.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
